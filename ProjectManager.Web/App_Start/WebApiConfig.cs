dfeaf205ee10479785be751ca1a53e237afe4f39
Paul Green <paulgreen55555@gmail.com>
2018-11-10 22:26:26 +0300

    Fixes fot getting json data from UI.

--- ProjectManager.Web/App_Start/WebApiConfig.cs
+++ ProjectManager.Web/App_Start/WebApiConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace ProjectManager.Web
 {
@@ -21,6 +22,9 @@ namespace ProjectManager.Web
                 routeTemplate: "{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
 
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }
